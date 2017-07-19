5789d3874ba1131d6fee7152ba6b79fa0a68c540
Jack Young <youngjack86@gmail.com>
2017-07-19 16:05:23 +0800

    almost rewritten this short script for better performace
    Now it supports multiple "../" or "./" at any position.

--- abspath.sh
+++ abspath.sh
@@ -1,58 +1,56 @@
 #!/bin/sh
 if [ $# -eq 0 ]
-	then
-	echo -e "\n\e[01;35m"$(pwd)"\e[0;00m\t\t[ Current Working DIR ]\n"
-	else
-	echo $1 | awk -F "/" 'BEGIN{
-			"pwd" | getline current_path;
-			len=split(current_path,cp,"/");
-			} 
-			{
-				a=len;
-				i=1;
-				b="";
-				p="";
-			if($0~/\.\./) {
-				while($i~/\.\./){
-					a=a-1;
-					i=i+1;
-					}
-				}else if($0~/\.\//){
-				i=i+1;	
-				}
-			for(j=i;j<=NF;j++){
-				if($j~/^\.$/){
-					next;
-					}
-				b=b"/"$j;
-				}
-			for(k=2;k<=a;k++){
-				p=p"/"cp[k];
-				}
-			gsub(/\/+/,"/",b);
-			gsub(/\/+/,"/",p);
-			if(($1 ne "") || (length($1)>0)){
-				"file "p""b | getline retrn;
-				len = split(retrn,arr,": ");
-				if(arr[len]~/No such file or directory/){
-					print "\n\033[05;45m"p""b"\033[0;00m\t\t[ Not Exist ]\n\n";
-					}else if(arr[len]~/directory$/){
-					print "\n\033[01;35m"p""b"\033[0;00m\t\t[ A Directory ]\n\n";
+then
+echo -e "\n\e[01;35m"$(pwd)"\e[0;00m\t\t[ Current Working DIR ]\n"
+else
+for i in $@;do
+	echo $i | awk -F "/" 'BEGIN{
+	"pwd" | getline current_path;
+} 
+{
+	if(($1 ne "") || (length($1)>0)){
+		pathall = current_path"/"$0;
+	}else{
+		pathall = $0;
+	}
+	gsub(/\/+/,"/",pathall);
+	len=split(pathall,arr,"/");
+	if(pathall~/\.\./){ 
+		for(x=1;x<=len;x++){
+			tmp = x;
+			if(arr[x]~/\.\./){
+				arr_cons[x] = 2;
+				while(tmp>0){
+					if(arr_cons[tmp-1]==2){
+						tmp --;
 					}else{
-					split(arr[len],disc,",");
-					print "\n\033[01;35m"p""b"\033[0;00m\n\n[ A File: "disc[1]" ]\n";
+						arr_cons[tmp-1] = 2;
+						tmp = 0;
 					}
-				}else{
-				"file "b | getline retrn;
-				len = split(retrn,arr,": ");
-                                if(arr[len]~/No such file or directory/){
-                                        print "\n\033[05;45m"b"\033[0;00m\t\t[ Not Exist ]\n\n";
-                                        }else if(arr[len]~/directory$/){
-                                        print "\n\033[01;35m"b"\033[0;00m\t\t[ A Directory ]\n\n";
-                                        }else{
-                                        split(arr[len],disc,",");
-                                        print "\n\033[01;35m"b"\033[0;00m\n\n[ A File: "disc[1]" ]\n";
-                                        }
 				}
-		}'
-	fi
+			}
+			if(arr[x] == "."){
+				arr_cons[x] = 2;
+			}
+		}
+	}
+	for(y=1;y<=len;y++){
+		if(arr_cons[y] == 2){
+			arr[y] = "";
+		}
+		pathFinal = pathFinal"/"arr[y];
+	}
+	gsub(/\/+/,"/",pathFinal);
+	"file "pathFinal | getline retrn;
+	len = split(retrn,arr,": ");
+	if(arr[len]~/No such file or directory/){
+		print "\n\033[05;45m"pathFinal"\033[0;00m\t\t[ Not Exist ]\n\n";
+	}else if(arr[len]~/directory$/){
+		print "\n\033[01;35m"pathFinal"\033[0;00m\t\t[ A Directory ]\n\n";
+	}else{
+		split(arr[len],disc,",");
+		print "\n\033[01;35m"pathFinal"\033[0;00m\n\n[ A File: "disc[1]" ]\n";
+	}
+}'
+done
+fi
